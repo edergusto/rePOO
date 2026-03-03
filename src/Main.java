a362c8f46e3ae5cc87bb37b33ebc0db3d75b453e
Eder Augusto <ederguzto@gmail.com>
2026-03-02 21:26:13 -0400

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
+import br.com.petshop.Pet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+        List<Pet> pets = new ArrayList<Pet>();
+        boolean sair = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------------- UniPetShop ---------------");
+        do{
+            System.out.println("-                   MENU                  -");
+            System.out.println("1. Cadastrar Pet | 2. Exibir Pets | 3. Sair");
+            int escolha = sc.nextInt();
+            switch (escolha){
+                case 1:
+                    cadastrarPet(pets);
+                    break;
+                case 2:
+                    exibirPetsCadastradas(pets);
+                    break;
+                case 3:
+                    System.out.println();
+                    sair = true;
+                    break;
+            }
+        }while (!sair);
+    }
+
+    public static void cadastrarPet(List<Pet> pets){
+        Scanner sc = new Scanner(System.in);
+        Pet pet = new Pet();
+        System.out.println("Nome:");
+        pet.nome = sc.nextLine();
+        System.out.println("Peso:");
+        pet.peso = sc.nextFloat();
+        System.out.println("Raça:");
+        pet.raca = sc.nextLine();
+        System.out.println("Idade:");
+        pet.idade = sc.nextInt();
+        System.out.println("Cadastrado!");
+        pets.add(pet);
+    }
+
+    public static void exibirPetsCadastradas(List<Pet> pets){
+        for (Pet pet : pets){
+            System.out.println(pet.nome);
+        }
     }
 }
---
+++ src/br/com/petshop/Pet.java
@@ -0,0 +1,8 @@
+package br.com.petshop;
+
+public class Pet {
+    public String nome;
+    public int idade;
+    public String raca;
+    public float peso;
+}
